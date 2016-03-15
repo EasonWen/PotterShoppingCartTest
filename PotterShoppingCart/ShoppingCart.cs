c6e941ed7333eeb6d6ddbf1317016516a68a377e
EasonWen <jossisgood@gmail.com>
2016-03-15 23:00:39 +0800

    workagin

---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        private List<int> _order = new List<int>();
+
+        public double CheckOut()
+        {
+            double totalPrice= 0; 
+            
+            while (_order.Count > 0)
+            {
+                var uniqueItems = new HashSet<int>(_order); //過濾連續買的集數
+                var discount = Discount(uniqueItems.Count); //折扣
+                foreach (var item in uniqueItems)
+                {
+                    _order.Remove(item);
+                }
+
+                totalPrice += (100 * uniqueItems.Count) * discount;
+            }
+
+            return totalPrice;
+        }
+
+        public void AddBookSetToCar(int bookSetNum)
+        {
+            _order.Add(bookSetNum);
+        }
+
+        private double Discount(int bookAmount)
+        {
+            double discount = 1;
+
+            switch (bookAmount)
+            {
+                case 2:
+                    return discount = 0.95;
+                case 3:
+                    return discount = 0.90;
+                case 4:
+                    return discount = 0.80;
+                case 5:
+                    return discount = 0.75;
+            }
+            return discount;
+        }
+
+
+
+    }
+
+}
---
+++ PotterShoppingCartTest/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+
+namespace PotterShoppingCartTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Buy_one_should_be_100()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected=100; 
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+               
+            //act
+            var actual = shoppingCar.CheckOut();   
+            //assertrt
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_should_be_190()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 190;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_three_should_be_270()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 270;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_three_four_should_be_320()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 320;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+            shoppingCar.AddBookSetToCar(4);  //加入第四集
+
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_three_four_five_should_be_375()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 375;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+            shoppingCar.AddBookSetToCar(4);  //加入第四集
+            shoppingCar.AddBookSetToCar(5);  //加入第五集
+
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_three_three_should_be_370()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 370;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+    
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Buy_one_two_two_three_three_should_be_460()
+        {
+            //arrange
+            var shoppingCar = new ShoppingCart();
+            var expected = 460;
+            shoppingCar.AddBookSetToCar(1);  //加入第一集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(2);  //加入第二集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+            shoppingCar.AddBookSetToCar(3);  //加入第三集
+
+            //act
+            var actual = shoppingCar.CheckOut();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+    
+}
